168c31e6eceff1063545e122b28699ba6e0d1e9b
Daro Marenco <jesusmarencodev@gmail.com>
2020-09-22 08:00:57 -0500

    subiendo3

---
+++ docs/precache-manifest.ef4dbbf5455fcda26a6072105af35333.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3fd87aefcea352714797342e6e23d18",
+    "url": "./index.html"
+  },
+  {
+    "revision": "641748894769d87632fa",
+    "url": "./static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "1fedca7e974bd82cd3c4",
+    "url": "./static/css/main.41aa9e2a.chunk.css"
+  },
+  {
+    "revision": "641748894769d87632fa",
+    "url": "./static/js/2.c4037b2a.chunk.js"
+  },
+  {
+    "revision": "590af14d9bb9b4b1926797b2a32e33a1",
+    "url": "./static/js/2.c4037b2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1fedca7e974bd82cd3c4",
+    "url": "./static/js/main.acc8d614.chunk.js"
+  },
+  {
+    "revision": "800e45d0c99aa020045c",
+    "url": "./static/js/runtime-main.3ce7ed12.js"
+  }
+]);
